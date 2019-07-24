520ff69f0228ae2c3f84d984a94cc9f3e3453ff3
Girish Palya <girishji@gmail.com>
2019-07-24 23:19:02 +0200

    Initial Commit

---
+++ Anki Assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anki Assistant
+//
+//  Created by gpalya on 7/24/19.
+//  Copyright © 2019 gpalya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
