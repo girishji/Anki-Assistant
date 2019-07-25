9b99aea007cb6f07ebc6c4283cfb832bb10102b2
Girish Palya <girishji@gmail.com>
2019-07-25 17:20:10 +0200

    using location services

--- Anki Assistant/ViewController.swift
+++ Anki Assistant/ViewController.swift
@@ -12,27 +12,32 @@ import CoreLocation
 class ViewController: UIViewController {
 
     var locationManager: CLLocationManager?
-    var timerStopped = true
+    let queue = DispatchQueue(label: "com.girish.ankiassistant")
+    // cannot read toggleSwitch.isOn from async thread, without causing thread backtrace
+    // and error in nslog, although after 15 seconds everything will start running fine again.
+    // But if you use dispatchque of main loop to read toggleSwitch, OS gives control back to
+    // main thread and background task stops.
+    // To avoid this, use a local variable and connect it to toggleSwitch state
+    var stopBackgroundTask = true
     
     //MARK: Properties
    
+    @IBOutlet weak var toggleSwitch: UISwitch!
     @IBOutlet weak var statusLabel: UILabel!
     
     //MARK: Actions
     
     @IBAction func switchValueChanged(_ sender: UISwitch, forEvent event: UIEvent) {
         if sender.isOn {
-            timerStopped = false
-            if locationManager == nil {
-                locationManager = CLLocationManager()
-                startReceivingLocationChanges()
-            } else {
-                locationManager?.startUpdatingLocation()
-            }
-        } else { // switch is OFF
-            timerStopped = true
-            locationManager?.stopUpdatingLocation()
-        }
+            configureLocationManager()
+            startReceivingLocationChanges()
+            stopBackgroundTask = false
+            //toggleSwitch.setOn(false, animated: true)
+            //toggleSwitch
+        } //else { // switch is OFF
+          //  locationManager?.stopUpdatingLocation()
+          //  stopBackgroundTask = true
+        //}
     }
     
     
@@ -42,18 +47,24 @@ class ViewController: UIViewController {
         statusLabel.text = "Anki Assistant"
     }
     
-    
     // girish
-    // to fake background task, use location manager
+    
+    func configureLocationManager() {
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager?.distanceFilter = 100000000.0  // In meters.
+            locationManager?.delegate = self
+            locationManager?.requestWhenInUseAuthorization()
+            locationManager?.requestAlwaysAuthorization()
+        }
+    }
+    
+    // to fake background task, use location manager in a async thread
     func startReceivingLocationChanges() {
 
         // Configure and start the service.
-        locationManager!.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager!.distanceFilter = 100000000.0  // In meters.
-        locationManager!.delegate = self
-        locationManager!.requestWhenInUseAuthorization()
-        locationManager!.requestAlwaysAuthorization()
-
+        
         let authorizationStatus = CLLocationManager.authorizationStatus()
         if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways {
             // User has not authorized access to location information.
@@ -75,18 +86,22 @@ extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
         //let lastLocation = locations.last!
-        //let MaxDuration = 24 * 60 * 60 // one day in seconds
-        //for _ in 0..<MaxDuration {
-        if !timerStopped {
-            NSLog("event")
-            sleep(1)
-            NSLog("after")
-            // stopping and starting is better than a loop with a sleep as it will give
-            // this thread time to process switch on/off events from UI
-            manager.stopUpdatingLocation()
-            manager.startUpdatingLocation()
-        } else {
-            manager.stopUpdatingLocation()
+        queue.async {
+            let MaxDuration = 12 * 60 * 60 // 12 hours in seconds
+            for _ in 0..<MaxDuration {
+                if !self.stopBackgroundTask {
+                    NSLog("event")
+                    sleep(1)
+                    NSLog("after")
+                    // stopping and starting updatenotification does not deliver events
+                    // when app is in background. So, use for loop with sleep
+                    //manager.stopUpdatingLocation()
+                    //manager.startUpdatingLocation()
+                } else {
+                    manager.stopUpdatingLocation()
+                    return
+                }
+            }
         }
     }
     
@@ -106,3 +121,33 @@ extension ViewController: CLLocationManagerDelegate {
         return .lightContent
     }
 }
+
+// girish
+// UIApplication is a singleton
+//                if UIApplication.shared.applicationState == .active {
+//                    NSLog("active")
+//                } else if UIApplication.shared.applicationState == .inactive {
+//                    NSLog("inactive")
+//                } else if UIApplication.shared.applicationState == .background {
+//                    NSLog("background")
+//                } else {
+//                    NSLog("unknown")
+//
+// state does not change from 'active' when you background the task
+//   if it is blocked in a sleep loop.
+
+//
+// cannot read toggleSwitch.isOn from async thread, without causing thread backtrace
+// and error in nslog, although after 15 seconds everything will start running fine again.
+// But if you use dispatchque of main loop to read toggleSwitch, OS gives control back to
+// main thread and background task stops.
+// To avoid this, use a local variable and connect it to toggleSwitch state
+//                var toggleSwitchIsOn = false
+//                DispatchQueue.main.async {
+//                    //Do UI Code here.
+//                    toggleSwitchIsOn = self.toggleSwitch.isOn
+//                }
+// above causes OS to give control back to main thread and background task stops
+//
+// asych queues don't work when app is backgrounded
+//
